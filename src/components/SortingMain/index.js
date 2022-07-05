f9a0266c7cba68fe383ad8ab762df838233726da
KW <kenneth_sl_wong@msn.com>
2022-07-05 19:19:56 -0400

    added basic styling with bootstrap and ability to display array

--- src/components/SortingMain/index.js
+++ src/components/SortingMain/index.js
@@ -1,24 +1,29 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 function SortingMain() {
     const [state, setState] = useState([]);
 
     useEffect(() => {
         newArray();
-    }, [] );
+    }, []);
 
     const newArray = () => {
         const array = [];
-        for(let i = 0; i <= 101; i++){
-            array.push(Math.floor(Math.random()*500)+10);
+        for (let i = 0; i <= 200; i++) {
+            array.push(Math.floor(Math.random() * 500) + 10);
         }
         setState(array);
     }
-    
+
     return (
-        <section>
-            <h1>Sorter</h1>
-        </section>
+        <div>
+            <h1 className="text-center my-2">Sorter</h1>
+            <div className="array-container">
+                {state.map((value, i) => (
+                    <div className="array-value" key={i} style={{ height: `${value}px` }}></div>
+                ))}
+            </div>
+        </div>
     )
 }
 
