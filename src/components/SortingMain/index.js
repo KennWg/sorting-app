af3d9af154c0cc9d7a3ba2ef61ac88b055e2425e
KW <kenneth_sl_wong@msn.com>
2022-07-06 15:06:03 -0400

    added buttons

--- src/components/SortingMain/index.js
+++ src/components/SortingMain/index.js
@@ -17,7 +17,14 @@ function SortingMain() {
 
     return (
         <div className="w-100 justify-content-center">
-            <h1 className="text-center my-2">Sorter</h1>
+            <h1 className="text-center my-3">Sorter</h1>
+            <div className="row w-100 pb-4 justify-content-center">
+                <button className="col-3 btn btn-primary mx-5" onClick={newArray}>Generate New Array</button>
+                <button className="col-2 btn btn-info mx-1">Merge Sort</button>
+                <button className="col-2 btn btn-info mx-1">Heap Sort</button>
+                <button className="col-2 btn btn-info mx-1">Quick Sort</button>
+                <button className="col-2 btn btn-info mx-1">Bubble Sort</button>
+            </div>
             <div className="array-container py-2 w-100 justify-content-center d-flex align-items-end">
                 {state.map((value, i) => (
                     <div className="array-value" key={i} style={{ height: `${value}px` }}></div>
